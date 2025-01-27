30c2f1dec1dea8dda45fd70d116714e77f2aae8d
GautamGM <gautam.netweb@gmail.com>
2025-01-27 12:02:12 +0530

    add the smoth scrolling

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,38 @@
-import { UserList } from "@components"
-import { useDispatch } from "react-redux"
-import { getallUser } from "@store/slices/userSlice"
-import { useEffect } from "react"
+/* eslint-disable no-unused-vars */
+import { UserList } from "@components";
+import { useDispatch } from "react-redux";
+import { getallUser } from "@store/slices/userSlice";
+import { useEffect } from "react";
+import LocomotiveScroll from 'locomotive-scroll';
 function App() {
-const dispatch=useDispatch()
-useEffect(()=>{
-dispatch(getallUser())
-},[dispatch])
+  
+  const locomotiveScroll = new LocomotiveScroll();
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getallUser());
+  }, [dispatch]);
   return (
-   <>
-   <div>
-    <UserList/>
-   </div>
-   </>
-  )
+    <>
+      <div className=" w-[100%] h-[100vh]  bg-red-500">
+        <UserList />
+      </div>
+      <div className="h-[100vh] bg-green-500">
+        <UserList />
+      </div>
+      <div className="h-[100vh]">
+        <UserList />
+      </div>
+      <div className=" w-[100%] h-[100vh]  bg-red-500">
+        <UserList />
+      </div>
+      <div className="h-[100vh] bg-green-500">
+        <UserList />
+      </div>
+      <div className="h-[100vh]">
+        <UserList />
+      </div>
+    </>
+  );
 }
 
-export default App
+export default App;
--- src/components/userList/UserList.jsx
+++ src/components/userList/UserList.jsx
@@ -10,7 +10,7 @@ const UserList = () => {
   };
 console.log(data,"data of theiser")
   return (
-    <div className="p-6 bg-gray-100 min-h-screen">
+    <div className="p-6 min-h-screen">
       <h1 className="text-2xl font-bold text-gray-800 mb-4">User List</h1>
       <div className="space-y-4">
         {data && data.length > 0 ? (
